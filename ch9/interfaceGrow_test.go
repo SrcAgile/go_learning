3190b1366f7cc8c8d7a0649e57964e7970efa4dc
srcagile <2406714426@qq.com>
2019-04-25 19:46:54 +0800

    [err] 01- the interface variable only recieve the pointer type

---
+++ ch9/interfaceGrow_test.go
@@ -0,0 +1,37 @@
+package interface_prac_1
+
+import (
+	"fmt"
+	"testing"
+)
+
+type code string
+
+type programmer interface {
+	writeHelloWorld() code
+}
+
+type goProgrammer struct {
+}
+
+func (goer *goProgrammer) writeHelloWorld() code {
+	return "fmt.println helloworld"
+}
+
+type javaProgrammer struct {
+}
+
+func (jaer *javaProgrammer) writeHelloWorld() code {
+	return "system.out.println helloworld "
+}
+
+func sayHelloWorld(p programmer) {
+	fmt.Println(p.writeHelloWorld())
+}
+
+func TestInterface(t *testing.T) {
+	goer := goProgrammer{}
+	javer := javaProgrammer{}
+	sayHelloWorld(goer)
+	sayHelloWorld(javer)
+}
