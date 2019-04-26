c0dabe7b609f9dce28d1a4a88295299b7bdb41e8
srcagile <2406714426@qq.com>
2019-04-26 23:02:54 +0800

    [Known] the meaning of import

--- src/ch11/package_test/package_test.go
+++ src/ch11/package_test/package_test.go
@@ -3,6 +3,11 @@ package testpkg
 import "testing"
 import "ch11/pkg1"
 
+//the import "ch11/pkg1" command means finding package in the file which belong
+//to this direcotory
+//即寻找当前目录下所有go文件中的package文件，go文件叫什么名字都无所谓
+//而且同一目录下只能存在一个package，ch11/pkg1下的所有go文件中的package要一致
+
 func TestPkg(t *testing.T) {
 	news.Spreading()
 }
