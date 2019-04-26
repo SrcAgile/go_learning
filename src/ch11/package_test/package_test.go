397abc8d6aecbf1f40dd4877aea9cc9c3be30425
srcagile <2406714426@qq.com>
2019-04-26 22:59:10 +0800

    [new]05 use package to manage project

---
+++ src/ch11/package_test/package_test.go
@@ -0,0 +1,8 @@
+package testpkg
+
+import "testing"
+import "ch11/pkg1"
+
+func TestPkg(t *testing.T) {
+	news.Spreading()
+}
---
+++ src/ch11/pkg1/pkg.go
@@ -0,0 +1,7 @@
+package news
+
+import "fmt"
+
+func Spreading() {
+	fmt.Println("bad news: you have been recived by THU")
+}
