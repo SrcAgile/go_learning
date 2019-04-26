b99a785ece1ef622df7ebafa71bfe7edc2eea13e
srcagile <2406714426@qq.com>
2019-04-26 21:38:00 +0800

    [refactor]01 empty interface

---
+++ ch10/interface_multitype/interfaceMul_test.go
@@ -0,0 +1,28 @@
+package empty_interface
+
+import (
+	"fmt"
+	"testing"
+)
+
+func DoSomething(in interface{}) {
+	switch v := in.(type) {
+	case int:
+		fmt.Println("i am int and val is", v)
+	case string:
+		fmt.Println("i am strign and val is", v)
+	default:
+		fmt.Println("i donot know who i am")
+	}
+
+}
+
+func TestEmptyInterface(t *testing.T) {
+	var a int
+	a = 10
+	DoSomething(a)
+
+	var b string
+	b = "HelloWorld"
+	DoSomething(b)
+}
