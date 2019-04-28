aa647a568848952e136b99b30e32ba084ed6eba7
srcagile <2406714426@qq.com>
2019-04-28 16:14:45 +0800

    [Known] a better way to prevent main thread quiting

--- src/ch12/coroutine/coroutine_test.go
+++ src/ch12/coroutine/coroutine_test.go
@@ -4,23 +4,25 @@ import (
 	"fmt"
 	"sync"
 	"testing"
-	"time"
 )
 
 func TestCoroutine(t *testing.T) {
 	var mutex sync.Mutex
-
+	var wg sync.WaitGroup
 	count := 0
 	for i := 0; i < 10000; i++ {
 
+		wg.Add(1)
 		go func() {
 			defer mutex.Unlock()
 			mutex.Lock()
 			count = count + 1
 			fmt.Println("the count is", count)
+			wg.Done()
 		}()
 
 	}
+	wg.Wait()
 	//防止主线程提前退出
-	time.Sleep(1 * time.Second)
+	//time.Sleep(1 * time.Second)
 }
