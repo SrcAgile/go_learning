17a130380ce8bb62d6effd19ccc9017483c3ac2a
srcagile <2406714426@qq.com>
2019-04-28 16:10:06 +0800

    [new]07 use coroutine

---
+++ src/ch12/coroutine/coroutine_test.go
@@ -0,0 +1,26 @@
+package corotine_test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCoroutine(t *testing.T) {
+	var mutex sync.Mutex
+
+	count := 0
+	for i := 0; i < 10000; i++ {
+
+		go func() {
+			defer mutex.Unlock()
+			mutex.Lock()
+			count = count + 1
+			fmt.Println("the count is", count)
+		}()
+
+	}
+
+	time.Sleep(1 * time.Second)
+}
